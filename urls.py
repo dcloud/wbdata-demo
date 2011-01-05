fc7a36dbafec678433ffb5c5c461916903e6db53
Daniel <daniel@danielcloud.org>
2011-01-05 17:47:14 -0500

    Initial project settings. Since this is a demo, this is a django project which will contain an application that ordinarily would be developed and packaged using setuptools or the like. Using a sqlite db, and no third party apps in use yet. Static file handling coming soon.

---
+++ urls.py
@@ -0,0 +1,24 @@
+from django.conf.urls.defaults import *
+from django.conf import settings
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Example:
+    # (r'^wbdata_demo/', include('wbdata_demo.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    (r'^admin/', include(admin.site.urls)),
+)
+
+
+# If in debug mode, we use django to serve images
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': '/Users/daniel_cloud/Sites/glocalstories_static_media'}),
+    )
