f6846d74e3117a131da28f8a8d0489da16c45c40
Daniel <daniel@danielcloud.org>
2011-01-10 00:58:22 -0500

    Add pagination to the country object page. Make view find most recent year of data for a particular indicator (Some indicators make not have data in the current year, especially for a particular country)

--- wbdata/views.py
+++ wbdata/views.py
@@ -6,12 +6,23 @@ from models import *
 from datetime import datetime
 
 def country_object_detail(request, country_id, year=None, template_name='wbdata/country_object_detail.html'):
-    if not year:
-        years = DataPoint.objects.all().aggregate(Max('year'))
-        year = years['year__max']
     country = get_object_or_404(Country, id=country_id.upper())
+    if not year:
+        years =  country.datapoint_set.aggregate(Max('year'))
+        year = years['year__max']
     # datapoint_list = country.datapoint_set.filter(year__range=(begin_year, end_year)).order_by('indicator__id', '-year')
     datapoints = country.datapoint_set.filter(year=year).order_by('indicator__id')
+    
+    paginator = Paginator(datapoints, 20)
+    try:
+        page = int(request.GET.get('page', '1'))
+    except ValueError:
+        page = 1
+        
+    try:
+        datapoints = paginator.page(page)
+    except (EmptyPage, InvalidPage):
+        datapoints = paginator.page(paginator.num_pages)
     
     return render_to_response(template_name, {'country': country, 'datapoints': datapoints, 'year': year}, context_instance=RequestContext(request))
 
@@ -29,14 +40,14 @@ def country_indicator_list(request, country_id, indicator_id, template_name='wbd
     
 
 def indicator_object_detail(request, indicator_id, year=None, template_name='wbdata/indicator_datapoint_list.html'):
-    if not year:
-        max_year = DataPoint.objects.all().aggregate(Max('year'))
-        year = max_year['year__max']
 
     indicator = get_object_or_404(Indicator, id=indicator_id)
+    if not year:
+        max_year = indicator.datapoint_set.aggregate(Max('year'))
+        year = max_year['year__max']
     datapoint_list = indicator.datapoint_set.filter(year=year).order_by('indicator__id')
     
-    paginator = Paginator(datapoint_list, 25)
+    paginator = Paginator(datapoint_list, 20)
     try:
         page = int(request.GET.get('page', '1'))
     except ValueError:
